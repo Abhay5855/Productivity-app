3e46d93f2d3d93875c3078faf2576a9531fe58ee
Abhay5855 <ap3919529@gmail.com>
2022-01-20 23:44:54 +0530

    docs :add form to signup component

--- productivity-app/src/components/auth/Signup.js
+++ productivity-app/src/components/auth/Signup.js
@@ -2,13 +2,31 @@
 
 
 import React from 'react';
+import './signup.css'
+import '../global/global.css'
 
 const Signup = () => {
   return(
 
       <>
 
-         <main>
+         <main className='signup__container'>
+
+             
+             <form>
+
+               <input type='email' required placeholder='Enter Email here'/>
+               <input type='password' required placeholder='Enter Password'/>
+
+               <button type='submit'>Signup</button>
+               
+             </form>
+
+
+
+
+
+
 
 
 
