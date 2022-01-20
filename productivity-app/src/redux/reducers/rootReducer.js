e68a9a8723c6ee1d26a3bac392dc995d0d0d927e
Abhay5855 <ap3919529@gmail.com>
2022-01-21 01:13:49 +0530

    feat : include the firebaseReducer into the root reducer

--- productivity-app/src/redux/reducers/rootReducer.js
+++ productivity-app/src/redux/reducers/rootReducer.js
@@ -2,7 +2,7 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer';
 import {firestoreReducer} from 'redux-firestore'
-
+import { firebaseReducer } from 'react-redux-firebase';
 
 
 
@@ -10,6 +10,7 @@ const rootReducer = combineReducers({
 
       auth : authReducer,
       firestore : firestoreReducer,
+      firebase : firebaseReducer,
       
 })
 
