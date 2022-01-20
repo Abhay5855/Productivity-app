47baa014bd797d47d744cfd0f1bb2dd379af5e75
Abhay5855 <ap3919529@gmail.com>
2022-01-20 22:31:29 +0530

    docs: create route folder and auth folder to handle routes and authentication

---
+++ productivity-app/src/routes/PrivateRoutes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PrivateRoutes() {
+  return <div></div>;
+}
+
+export default PrivateRoutes;
---
+++ productivity-app/src/routes/Routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Routes() {
+  return <div></div>;
+}
+
+export default Routes;
