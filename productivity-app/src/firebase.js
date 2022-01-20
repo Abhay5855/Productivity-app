99baf40ab8d879bbe74c1397b754eac547c25081
Abhay5855 <ap3919529@gmail.com>
2022-01-21 00:28:57 +0530

    docs : pass the rootReducer to the store as function

--- productivity-app/src/firebase.js
+++ productivity-app/src/firebase.js
@@ -1,22 +1,14 @@
-
-
 import firebase from "firebase/app";
 import "firebase/auth";
 
-
 const app = firebase.initializeApp({
-
   apiKey: process.env.FIREBASE_APP_KEY,
   authDomain: process.env.FIREBASE_APP_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_APP_PROJECTID,
   storageBucket: process.env.FIREBASE_APP_STORAGE,
   messagingSenderId: process.env.FIREBASE_APP_MESSAGING_SENDER,
   appId: process.env.FIREBASE_APP_ID,
-
-
-})
-
-
+});
 
 export const auth = app.auth();
 
--- productivity-app/src/redux/store.js
+++ productivity-app/src/redux/store.js
@@ -1,13 +1,12 @@
 
 
 import {createStore} from 'redux';
+import rootReducer from './reducers/rootReducer';
 
 
+const store = createStore(
 
-const store = createStore({
-
-
-});
+  rootReducer);
 
 
 export default store;
